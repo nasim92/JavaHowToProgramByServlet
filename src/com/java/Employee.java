be7434e7a2e0951a8662bf58c64ee5931c1773f3
NasimSalmani <nasimsalmani172@yahoo.com>
2016-02-29 12:24:34 +0330

    Refactor rest API

--- src/com/java/Employee.java
+++ src/com/java/Employee.java
@@ -1,24 +1,19 @@
 package com.java;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+public class Employee {
+    private final String firstName;
+    private final String lastName;
+    private final String socialSecurityNumber;
+    private double grossSales;
+    private double commissionRate;
 
-public class CommissionEmployeeServlet extends javax.servlet.http.HttpServlet {
-     private final String firstName;
-     private final String lastName;
-     private final String socialSecurityNumber;
-     private double grossSales;
-     private double commissionRate;
-
-    public CommissionEmployeeServlet(String firstName, String lastName, String socialSecurityNumber, double grossSales, double commissionRate)
+    public Employee(String firstName, String lastName, String socialSecurityNumber, double grossSales, double commissionRate)
     {
         if (grossSales < 0.0)
-             throw new IllegalArgumentException("Gross sales must be >= .");
+            throw new IllegalArgumentException("Gross sales must be >= .");
 
         if (commissionRate <= 0.0 || commissionRate >= 1.0)
-             throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
 
         this.firstName = firstName;
         this.lastName = lastName;
@@ -39,12 +34,12 @@ public class CommissionEmployeeServlet extends javax.servlet.http.HttpServlet {
         return socialSecurityNumber;
     }
 
-    public double getCommissionRate() {
-        return commissionRate;
-    }
-
     public double getGrossSales() {
         return grossSales;
+    }
+
+    public double getCommissionRate() {
+        return commissionRate;
     }
 
     public void setCommissionRate(double commissionRate) {
@@ -61,6 +56,13 @@ public class CommissionEmployeeServlet extends javax.servlet.http.HttpServlet {
         this.grossSales = grossSales;
     }
 
+    public double earnings()
+    {
+        return commissionRate * grossSales;
+
+    }
+
+
     @Override
     public String toString() {
         return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f",
@@ -70,8 +72,4 @@ public class CommissionEmployeeServlet extends javax.servlet.http.HttpServlet {
                 "commission rate", commissionRate);
     }
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-    }
 }
